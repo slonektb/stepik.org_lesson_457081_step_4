980883fe0a911eeaf4968905af4b243d7528657d
slonektb <gb_alex@mail.ru>
2021-07-31 13:21:35 +0500

    finish him

---
+++ src/Main.java
@@ -0,0 +1,55 @@
+import java.io.*;
+import java.util.Arrays;
+
+public class Main {
+    public static void main (String[] args) {
+        String str = "";
+        double result = 0;
+        try (FileReader reader = new FileReader("input.txt")) {
+            char[] buf = new char[256];
+            int c;
+            while((c = reader.read(buf))>0){
+
+                if(c < 256){
+                    buf = Arrays.copyOf(buf, c);
+                }
+            }
+            str = String.valueOf(buf);
+            result = maths(str.split(" "));
+            System.out.println("" + result);
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+
+    }
+
+    public static double maths (String[] arg) throws Exception{
+        double a;
+        double b;
+
+        try {
+            a = Double.parseDouble(arg[0]);
+            b = Double.parseDouble(arg[2]);
+        } catch (NumberFormatException e) {
+            throw new Exception("Error! Not number");
+        }
+
+        if (!arg[1].equals("+") && !arg[1].equals("-") && !arg[1].equals("*") && !arg[1].equals("/")) {
+            throw new Exception("Operation Error!");
+        }
+
+        if (arg[1].equals("+")) {
+            return a + b;
+        } else if (arg[1].equals("-")) {
+            return a - b;
+        } else if (arg[1].equals("*")) {
+            return a * b;
+        } else {
+            if (b == 0.0) {
+                throw new Exception("Error! Division by zero");
+            }
+            return a / b;
+        }
+    }
+
+}
